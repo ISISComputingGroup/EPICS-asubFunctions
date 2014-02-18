69a8eb0be1ad4bca432def80bbf04a83e1195bbe
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2014-02-18 14:55:34 +0000

    Write blanks to end of last string if not complete
    Former-commit-id: c6ee2b08f4246d34b84615a944b2f6272f2c08b0

--- asubFunctionsApp/src/charToStringWaveform.c
+++ asubFunctionsApp/src/charToStringWaveform.c
@@ -1,5 +1,6 @@
 /** @file charToStringWaveform.c
  *  @author Freddie Akeroyd, STFC (freddie.akeroyd@stfc.ac.uk)
+ *  @ingroup asub_functions
  *
  *  Copy a CHAR waveform record into a STRING waveform record. If this is done by
  *  a normal CAPUT the character byte codes are not preserved
@@ -12,27 +13,34 @@
 #include <aSubRecord.h>
 
 #include <epicsExport.h>
-
+/**
+ *  Convert a character waveform into a string waveform
+ *  @ingroup asub_functions
+ *  @param[in] prec Pointer to aSub record
+ */
 static long charToStringWaveform(aSubRecord *prec) 
 {
 	const char* str_in = (const char*)(prec->a); /* waveform CHAR data */
     epicsUInt32 len_in = *(epicsUInt32*)(prec->b); /* usually NORD from waveform */
     epicsOldString* str_out = (epicsOldString*)prec->vala; /* epicsOldString is typedef for epics fixed length string */ 
 	epicsUInt32 len_out = 0;
+	epicsUInt32 max_out_bytes = prec->nova * sizeof(epicsOldString);
     if (prec->noa < len_in) /* check input space */
 	{
 	    len_in = prec->noa;
 	}
     /*	Maybe could use prec->nea ? Think it is set by db link but not by CA */
-	if (prec->nova * sizeof(epicsOldString) < len_in) /* check output space */
+	if (max_out_bytes < len_in) /* check output space */
 	{
-	    len_in = prec->nova * sizeof(epicsOldString);
+	    len_in = max_out_bytes;
 	}
-	memcpy(str_out, str_in, len_in);
+	memcpy((char*)str_out, str_in, len_in);
 	if (len_in > 0)
 	{
 		len_out = 1 + (len_in - 1) / sizeof(epicsOldString);
 	}
+	memset((char*)str_out + len_in, ' ', len_out * sizeof(epicsOldString) - len_in); /* space pad rest of last string written to */
+	/* memset((char*)str_out + len_in, ' ', max_out_bytes - len_in); /* space pad all unused parts */
 	prec->neva = len_out;
     return 0; /* process output links */
 }
