23f8436baebc8e5baffaf0ba6d84cb17ca08bc70
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2014-07-02 10:23:19 +0000

    Add functions for handling "sets of things" via PVs - used in run control to keep a list of out of range PVs
    Former-commit-id: ecb0cc850f5c978f2adc8d97876a77bbf46a4996

---
+++ asubFunctionsApp/src/setFunctions.c
@@ -0,0 +1,90 @@
+/** @file setFunctions.c
+ *  @author Freddie Akeroyd, STFC (freddie.akeroyd@stfc.ac.uk)
+ *  @ingroup asub_functions
+ *
+ */
+#include <string.h>
+#include <registryFunction.h>
+#include <aSubRecord.h>
+#include <menuFtype.h>
+#include <errlog.h>
+
+#include <epicsExport.h>
+
+#include "setFunctions.h"
+
+/**
+ *  @ingroup asub_functions
+ *  @param[in] prec Pointer to aSub record
+ */
+static long addToSet(aSubRecord *prec) 
+{
+    epicsOldString* set_name = (epicsOldString*)prec->a; /* epicsOldString is typedef for epics fixed length string */ 
+	const char* item_value = (const char*)(prec->b); /* waveform CHAR data */
+    epicsUInt32 max_len_item_value = *(epicsUInt32*)(prec->c); /* usually NORD from waveform */
+    if (prec->fta != menuFtypeSTRING || prec->ftb != menuFtypeCHAR || prec->ftc != menuFtypeLONG)
+	{
+         errlogPrintf("%s incorrect input type. A (STRING), B (CHAR), C (LONG)", prec->name);
+		 return -1;
+	}
+    if (prec->nob < max_len_item_value) /* check input space - not sure ->neb always gets set */
+	{
+	    max_len_item_value = prec->nob;
+	}
+	addToSetImpl(*set_name, item_value, max_len_item_value);
+    return 0; /* process output links */
+}
+
+static long removeFromSet(aSubRecord *prec) 
+{
+    epicsOldString* set_name = (epicsOldString*)prec->a; /* epicsOldString is typedef for epics fixed length string */ 
+	const char* item_value = (const char*)(prec->b); /* waveform CHAR data */
+    epicsUInt32 max_len_item_value = *(epicsUInt32*)(prec->c); /* usually NORD from waveform */
+    if (prec->fta != menuFtypeSTRING || prec->ftb != menuFtypeCHAR || prec->ftc != menuFtypeLONG)
+	{
+         errlogPrintf("%s incorrect input type. A (STRING), B (CHAR), C (LONG)", prec->name);
+		 return -1;
+	}
+    if (prec->nob < max_len_item_value) /* check input space - not sure ->neb always gets set */
+	{
+	    max_len_item_value = prec->nob;
+	}
+	removeFromSetImpl(*set_name, item_value, max_len_item_value);
+    return 0; /* process output links */
+}
+
+static long getSetItemCount(aSubRecord *prec)
+{
+    epicsOldString* set_name = (epicsOldString*)prec->a; /* epicsOldString is typedef for epics fixed length string */ 
+    int item_count = 0;
+    if (prec->fta != menuFtypeSTRING || prec->ftva != menuFtypeLONG)
+	{
+         errlogPrintf("%s incorrect input type. A (STRING), VALA (LONG)", prec->name);
+		 return -1;
+	}
+    getSetItemCountImpl(*set_name, &item_count);
+    *(epicsUInt32*)prec->vala = item_count;
+    prec->neva = 1;
+    return 0; /* process output links */
+}
+
+static long getSetItems(aSubRecord *prec) 
+{
+    int item_count = 0;
+    epicsOldString* set_name = (epicsOldString*)prec->a; /* epicsOldString is typedef for epics fixed length string */ 
+	char* item_value = (char*)(prec->vala); /* waveform CHAR data */
+    if (prec->fta != menuFtypeSTRING || prec->ftva != menuFtypeCHAR || prec->ftvb != menuFtypeLONG)
+	{
+         errlogPrintf("%s incorrect input type. A (STRING), VALA (CHAR), VALB (LONG)", prec->name);
+		 return -1;
+	}
+	getSetItemsImpl(*set_name, item_value, prec->nova, &(prec->neva), &item_count);
+    *(epicsUInt32*)prec->valb = item_count;
+    prec->nevb = 1;
+    return 0; /* process output links */
+}
+
+epicsRegisterFunction(addToSet); /* must also be mentioned in asubFunctions.dbd */
+epicsRegisterFunction(removeFromSet); /* must also be mentioned in asubFunctions.dbd */
+epicsRegisterFunction(getSetItems); /* must also be mentioned in asubFunctions.dbd */
+epicsRegisterFunction(getSetItemCount); /* must also be mentioned in asubFunctions.dbd */
---
+++ asubFunctionsApp/src/setFunctionsImpl.cpp
@@ -0,0 +1,87 @@
+/// @file setFunctions.c
+/// @author Freddie Akeroyd, STFC (freddie.akeroyd@stfc.ac.uk)
+/// @ingroup asub_functions
+///
+///
+#include <map>
+#include <set>
+#include <string>
+
+#include <epicsGuard.h>
+#include <epicsMutex.h>
+
+#include "setFunctions.h"
+
+static std::map< std::string, std::set<std::string> > the_sets;
+
+static epicsMutex set_lock;
+
+// look for a possible NULL terminator in bytes
+static int get_length(const char* str, int maxlen)
+{
+    int len;
+	for(len = 0; len < maxlen && str[len] != '\0'; ++len)
+	    ;
+	return len;
+}
+ 
+int addToSetImpl(const char* set_name, const char* item_value, int max_len_item_value)
+{
+    epicsGuard<epicsMutex> _lock(set_lock);
+    std::set<std::string>& the_set = the_sets[set_name];
+    std::string item(item_value, get_length(item_value, max_len_item_value));
+	the_set.insert(item);
+	return 0;
+}
+
+int removeFromSetImpl(const char* set_name, const char* item_value, int max_len_item_value)
+{
+    epicsGuard<epicsMutex> _lock(set_lock);
+    std::set<std::string>& the_set = the_sets[set_name];
+    std::string item(item_value, get_length(item_value, max_len_item_value));
+	the_set.erase(item);
+	return 0;
+}
+
+int	getSetItemCountImpl(const char* set_name, int* item_count)
+{
+    epicsGuard<epicsMutex> _lock(set_lock);
+    const std::set<std::string>& the_set = the_sets[set_name];
+	*item_count = the_set.size();
+	return 0;
+}
+
+int	getSetItemsImpl(const char* set_name, char* set_values, int max_len, int* len, int* item_count)
+{
+    epicsGuard<epicsMutex> _lock(set_lock);
+    const std::set<std::string>& the_set = the_sets[set_name];
+	int i = 0;
+    for(std::set<std::string>::const_iterator it = the_set.begin(); i < max_len && it != the_set.end(); ++it)
+	{
+	    strncpy(set_values + i, it->c_str(), max_len - i);
+		i += it->size();
+		if (i < max_len)
+		{
+		    set_values[i] = ' ';
+			++i;
+		}
+	}
+	if (i < max_len)
+	{
+	    if (i > 0)
+		{
+		    --i;   // remove trailing final ' ' added above
+		}
+	    memset(set_values + i, '\0', max_len - i); // or space pad?
+	    *len = i;
+	}
+	else
+	{
+	    set_values[max_len-1] = '\0';
+		*len = max_len-1;
+	}
+	*item_count = the_set.size();
+	return 0;
+}
+
+
