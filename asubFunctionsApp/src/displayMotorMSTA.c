09215ced5381d3b79ff0281857ed84f44d54b126
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2014-07-06 16:00:25 +0000

    Add MSTA string status field
    Former-commit-id: eb5625f436d66dab54412ec1eb2c404a838cf404

--- asubFunctionsApp/src/displayMotorMSTA.c
+++ asubFunctionsApp/src/displayMotorMSTA.c
@@ -27,32 +27,32 @@ static long displayMotorMSTA(aSubRecord *prec)
 		 return -1;
 	}
 	buffer[0] = '\0';
-	strcat(buffer, ((msta & 0x1) ? "LAST_RAW_DIR: POS, " : "LAST_RAW_DIR: NEG, ") );
-	strcat(buffer, ((msta & 0x2) ? "MOT_DONE: YES, " : "MOT_DONE: NO, ") );
-	strcat(buffer, ((msta & 0x4) ? "PLUS_LS, " : "") );
-	strcat(buffer, ((msta & 0x8) ? "HOMELS: YES " : "") );
-/*	strcat(buffer, ((msta & 0x10) ? "DONE: YES" : "DONE: NO") ); not used*/
-	strcat(buffer, ((msta & 0x20) ? "POS: closed_loop, " : "") );
+	strcat(buffer, ((msta & 0x400) ? "MOVING, " : "") );
+	strcat(buffer, ((msta & 0x2) ? "DONE, " : "") );
 	strcat(buffer, ((msta & 0x40) ? "SLIP_STALL, " : "") );
-	strcat(buffer, ((msta & 0x80) ? "AT_HOME, " : "") );
-	strcat(buffer, ((msta & 0x100) ? "ENCODER: YES, " : "ENCODER: NO, ") );
 	strcat(buffer, ((msta & 0x200) ? "PROBLEM, " : "") );
-	strcat(buffer, ((msta & 0x400) ? "MOVING: YES, " : "MOVING: NO, ") );
-	strcat(buffer, ((msta & 0x800) ? "GAIN_SUPPORT, " : "") );
 	strcat(buffer, ((msta & 0x1000) ? "COMM_ERR, " : "") );
 	strcat(buffer, ((msta & 0x2000) ? "MINUS_LS, " : "") );
+	strcat(buffer, ((msta & 0x8) ? "HOMELS, " : "") );
+	strcat(buffer, ((msta & 0x4) ? "PLUS_LS, " : "") );
+	strcat(buffer, ((msta & 0x80) ? "AT_HOME, " : "") );
+	strcat(buffer, ((msta & 0x1) ? "DIR: POS, " : "DIR: NEG, ") );
+/*	strcat(buffer, ((msta & 0x10) ? "DONE: YES" : "DONE: NO") ); not used*/
+	strcat(buffer, ((msta & 0x100) ? "ENCODER: YES, " : "ENCODER: NO, ") );
 	strcat(buffer, ((msta & 0x4000) ? "HOMED: YES" : "HOMED: NO") );
+	strcat(buffer, ((msta & 0x800) ? "GAIN_SUPPORT, " : "") );
+	strcat(buffer, ((msta & 0x20) ? "POS: closed_loop, " : "") );
 	n = strlen(buffer);
 	strncpy(msta_display, buffer, max_len);
 	if (n >= max_len)
 	{
 		msta_display[max_len-1] = '\0';
-		prec->neva = max_len - 1;
+		prec->neva = max_len;
 	}
 	else
 	{
 	    memset(msta_display + n, '\0', max_len - n); 
-		prec->neva = n;
+		prec->neva = n + 1; // n+1 to make sure '\0' is included
 	}
     return 0; /* process output links */
 }
