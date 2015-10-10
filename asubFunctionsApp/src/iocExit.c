b9942acb0f4e241d6a1821620e2feab831610587
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2015-10-10 13:55:46 +0100

    Use separate thread for exit

--- asubFunctionsApp/src/iocExit.c
+++ asubFunctionsApp/src/iocExit.c
@@ -10,17 +10,14 @@
 #include <menuFtype.h>
 #include <errlog.h>
 #include <epicsExit.h>
+#include <epicsThread.h>
 
 #include <epicsExport.h>
 
-/**
- *  @ingroup asub_functions
- *  @param[in] prec Pointer to aSub record
- */
-
-static long iocExit(aSubRecord *prec) 
+static void exitFunc(void* arg)
 {
-    long stat = *(long*)prec->a;
+    long stat = (long)arg;
+	epicsThreadSleep(0.1); /* give asub record time to finish */
     if (stat == 0)
 	{
 	    epicsExit(EXIT_SUCCESS);
@@ -29,6 +26,17 @@ static long iocExit(aSubRecord *prec)
 	{
 	    epicsExit(EXIT_FAILURE);
 	}
+}
+ 
+/**
+ *  @ingroup asub_functions
+ *  @param[in] prec Pointer to aSub record
+ */
+static long iocExit(aSubRecord *prec) 
+{
+    long stat = *(long*)prec->a;
+	epicsThreadCreate("iocExit", epicsThreadPriorityMedium, 
+	                  epicsThreadGetStackSize(epicsThreadStackMedium), exitFunc, (void*)stat);
     return 0; /* process output links */
 }
 
