72a1993daeaf92b04216b97dfca2b6a4cc035c94
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2015-11-04 19:08:37 +0000

    Return status value as well as string

--- asubFunctionsApp/src/checkMotorError.c
+++ asubFunctionsApp/src/checkMotorError.c
@@ -27,32 +27,47 @@ static long checkMotorError(aSubRecord *prec)
     epicsInt16 lls = *(epicsInt16*)(prec->d); 
     epicsEnum16 stat = *(epicsEnum16*)(prec->e); 
     epicsEnum16 sevr = *(epicsEnum16*)(prec->f); 
+	epicsOldString* mtr_name = (epicsOldString*)prec->g;
 	epicsOldString* str_out = (epicsOldString*)prec->vala;
+    epicsUInt32* stat_out = (epicsUInt32*)prec->valb;
     char* output = (char*)str_out;
     if (prec->fta != menuFtypeULONG || prec->ftb != menuFtypeSHORT || 
 	     prec->ftc != menuFtypeSHORT || prec->ftd != menuFtypeSHORT ||
-		 prec->fte != menuFtypeUSHORT || prec->ftf != menuFtypeUSHORT || prec->ftva != menuFtypeSTRING)
+		 prec->fte != menuFtypeUSHORT || prec->ftf != menuFtypeUSHORT || 
+		 prec->ftg != menuFtypeSTRING || prec->ftva != menuFtypeSTRING || prec->ftvb != menuFtypeULONG)
 	{
          errlogPrintf("%s incorrect input type. A (ULONG), VALA (CHAR)", prec->name);
 		 return -1;
 	}
     buffer[0] = '\0';
+#if 0
 	if ( (sevr != epicsSevNone) && (stat != epicsAlarmNone) )
 	{
 /*	    epicsSnprintf(buffer, sizeof(buffer), "ALARM %s %s", epicsAlarmSeverityStrings[sevr], epicsAlarmConditionStrings[stat]); */
 	    epicsSnprintf(buffer, sizeof(buffer), "ALARM ");
 	}
+#endif
 	if ( (msta & (1 << 6)) || (msta & (1 << 9)) || (msta & (1 << 12)) )
 	{
-	    strcat(buffer, "MOTOR_ERROR ");
+	    strcat(buffer, "ERROR ");
 	}
 	if ( (lvio == 1) || (hls != 0) || (lls != 0) || (msta & (1 << 2)) || (msta & (1 << 13)) )
 	{
-	    strcat(buffer, "AT_LIMIT ");	    
+	    strcat(buffer, "AT_LIMIT ");
 	}
-	strncpy(output, buffer, sizeof(epicsOldString));
-	output[sizeof(epicsOldString) - 1] = '\0';
+	if (strlen(buffer) > 0)
+	{
+	    epicsSnprintf(output, sizeof(epicsOldString), "%s %s", mtr_name[0], buffer);
+	    output[sizeof(epicsOldString) - 1] = '\0';
+		*stat_out = 1;
+	}
+	else
+	{
+	    memset(output, 0,  sizeof(epicsOldString));
+		*stat_out = 0;
+	}
 	prec->neva = 1;
+	prec->nevb = 1;
     return 0; /* process output links */
 }
 
