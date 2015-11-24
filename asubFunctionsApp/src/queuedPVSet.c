7dc2fe40a2e96a2338b158e8e32c990c72c59c1a
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2015-11-24 12:13:24 +0000

    Add queuedPVSet function
    This function allows for queued channel access puts with callback
    between IOCs

---
+++ asubFunctionsApp/src/queuedPVSet.c
@@ -0,0 +1,42 @@
+/** @file stringConcatWithCtrlB.c
+ *  @author Freddie Akeroyd, STFC (freddie.akeroyd@stfc.ac.uk)
+ *  @ingroup asub_functions
+ *
+ * expects A, B, C to be strings and D to be either a CHAR or DOUBLE waveform
+ * writes a char waveform to VALA
+ *
+ */
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <registryFunction.h>
+#include <aSubRecord.h>
+#include <menuFtype.h>
+#include <errlog.h>
+
+#include "queuedPVSetImpl.h"
+
+#include <epicsExport.h>
+/**
+ *  Concat several strings together with a \2 (^B) between each string 
+ *  @ingroup asub_functions
+ *  @param[in] prec Pointer to aSub record
+ */
+static long queuedPVSet(aSubRecord *prec) 
+{
+    char* pv_in = (char*)(prec->a); /* waveform CHAR data */
+    char* value_in = (char*)(prec->b); /* waveform CHAR data */
+	epicsFloat64 timeout = *(epicsFloat64*)(prec->c);
+    epicsUInt32 max_len_pv_in = prec->noa;
+    epicsUInt32 max_len_value_in = prec->nob;
+    epicsUInt32 len_pv_in = prec->nea;
+    epicsUInt32 len_value_in = prec->neb;
+    if (prec->fta != menuFtypeCHAR || prec->ftb != menuFtypeCHAR || prec->ftc != menuFtypeDOUBLE)
+	{
+         errlogPrintf("%s incorrect input type. A (CHAR), B (CHAR), C (DOUBLE)\n", prec->name);
+		 return -1;
+	}
+	return queuedPVSetImpl(prec->name, pv_in, len_pv_in, max_len_pv_in, value_in, max_len_value_in, len_value_in, timeout);
+}
+
+epicsRegisterFunction(queuedPVSet); /* must also be mentioned in asubFunctions.dbd */
---
+++ asubFunctionsApp/src/queuedPVSetImpl.cpp
@@ -0,0 +1,123 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <algorithm>
+#include <registryFunction.h>
+#include <aSubRecord.h>
+#include <menuFtype.h>
+#include <errlog.h>
+#include <epicsMutex.h>
+#include <epicsEvent.h>
+#include <epicsMessageQueue.h>
+
+#include "queuedPVSetImpl.h"
+
+#include "pv.h"
+
+struct PVItem
+{
+    char recname[256];
+    char name[256];
+    char value[1024];
+	double timeout;
+};
+
+static epicsMutex the_lock;
+
+static bool first_call = true;
+
+static epicsMessageQueue the_queue(30, sizeof(PVItem));
+
+static epicsEvent callback_event;
+
+static void queuedCallback(void *var, pvType type, unsigned count, pvValue *value, void *arg, pvStat status)
+{
+    PVItem* pitem = (PVItem*)arg;
+//	printf("queuedCallback %s \"%s\" = \"%s\"\n", pitem->recname, pitem->name, pitem->value);
+    callback_event.signal();
+}
+
+static void queuedPVThread(void* arg)
+{
+    PVItem item;
+	pvSystem *sys = newPvSystem( "ca" );
+    while( true ) 
+	{
+		if ( the_queue.receive(&item, sizeof(item)) == sizeof(item) )
+		{
+			try
+			{
+	            pvVariable *var = sys->newVariable( item.name );
+		        sys->pend(0.1, TRUE);
+//		        printf("queuedPVThread %s set \"%s\" = \"%s\" STARTING\n", item.recname, item.name, item.value);
+                if (var->putCallback(pvTypeCHAR, strlen(item.value), (pvValue*)item.value, queuedCallback, &item) != pvStatOK)
+				{
+		            errlogPrintf("queuedPVThread %s set \"%s\" = \"%s\" PUT ERROR\n", item.recname, item.name, item.value);
+				}
+		        sys->pend(0.1, TRUE);
+//				while( !callback_event.tryWait() )
+//				{
+//		            sys->pend(0.1, TRUE);
+//				}
+				if ( !callback_event.wait(item.timeout) )
+				{
+		            errlogPrintf("queuedPVThread %s set \"%s\" = \"%s\" WAIT TIMEOUT\n", item.recname, item.name, item.value);
+				}
+//				else
+//				{
+//					printf("queuedPVThread %s set \"%s\" = \"%s\" DONE\n", item.recname, item.name, item.value);
+//				}
+		        delete var;
+			}
+			catch(const std::exception& ex)
+			{
+                errlogPrintf("%s exception %s\n", item.recname, ex.what());
+			}
+//		    epicsThreadSleep(0.1);
+		}
+	}
+    delete sys;
+}
+
+int queuedPVSetImpl(const char* recname, const char* pv_in, epicsUInt32 len_pv_in, epicsUInt32 max_len_pv_in, const char* value_in, epicsUInt32 max_len_value_in, epicsUInt32 len_value_in, double timeout)
+{
+    PVItem item;
+	if (max_len_pv_in >= sizeof(item.name))
+	{
+        errlogPrintf("%s pv to set too long\n", recname);
+	    return -1;
+	}
+	if (max_len_value_in >= sizeof(item.value))
+	{
+        errlogPrintf("%s pv value too long\n", recname);
+	    return -1;
+	}
+	if (pv_in == NULL || pv_in[0] == '\0' || len_pv_in == 0)
+	{
+        errlogPrintf("%s pv name is empty\n", recname);
+	    return -1;
+	}
+	memset(&item, 0, sizeof(item));
+	strncpy(item.recname, recname, sizeof(item.recname));
+	strncpy(item.name, pv_in, std::min(len_pv_in, max_len_pv_in));
+	strncpy(item.value, value_in, std::min(len_value_in, max_len_value_in));
+	item.timeout = timeout;
+	int stat = the_queue.trySend(&item, sizeof(item));
+	if (first_call)
+	{
+		epicsGuard<epicsMutex> _lock(the_lock);
+		if (first_call)
+		{
+	        first_call = false;
+            if (epicsThreadCreate("queuedPVThread",
+                          epicsThreadPriorityMedium,
+                          epicsThreadGetStackSize(epicsThreadStackMedium),
+                          (EPICSTHREADFUNC)queuedPVThread, NULL) == 0)
+            {
+                errlogPrintf("%s unable to start thread for queuedPVThread\n", recname);
+                return -1;
+            }
+        }			
+	}
+    return stat;
+}
